a388632c6e7fc2f7e2cbac48c13764f801c66dbf
DevillordMCPE <maulaxe@yahoo.com>
2015-06-05 17:15:31 +0700

    Create More Higher Mobs Spawn Rate.js

---
+++ More Higher Mobs Spawn Rate.js
@@ -0,0 +1,14 @@
+/*
+                    Red Eagle General License
+                       Version 3, 05/06/2015
+                       
+  You Are Not Allowed To Copying or Using Our Code Without Our Permission.
+  You Are Allowed To Copying or Using Our Code If You Are Have A Permisson From Leader Or Mods Creator.
+  Don't Claim Our Mods To Be Yours.
+  Don't Moditified Out Mods To YOur Version.
+  
+  Mod Creator : DevilLord RedEagle / DevilLord
+  Mod Release Date : 05/06/2015
+  
+  By Team Red Eagle.
+*/
