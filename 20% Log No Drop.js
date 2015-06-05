892a091c87abdc63f66207075bcb8c2a9ca1eeeb
DevillordMCPE <maulaxe@yahoo.com>
2015-06-05 17:38:40 +0700

    v1.0.1

--- 20% Log No Drop.js
+++ 20% Log No Drop.js
@@ -1,28 +1,16 @@
 /*
-Red Eagle General License
-
-                       Version 3, 05/06/2015
-
-                       
-
-  You Are Not Allowed To Copying or Using Our Code Without Our Permission.
-
-  You Are Allowed To Copying or Using Our Code If You Are Have A Permisson From Leader Or Mods Creator.
-
-  Don't Claim Our Mods To Be Yours.
-
-  Don't Moditified Out Mods To YOur Version.
-
-  
-
-  Mod Creator : ItsPiko
-
-  Mod Release Date : 5 June 2015
-
-  
-
-  By Team Red Eagle.
-
+                    Red Eagle General License
+                       Version 3, 05/06/2015
+                       
+  You Are Not Allowed To Copying or Using Our Code Without Our Permission.
+  You Are Allowed To Copying or Using Our Code If You Are Have A Permisson From Leader Or Mods Creator.
+  Don't Claim Our Mods To Be Yours.
+  Don't Moditified Out Mods To YOur Version.
+  
+  Mod Creator : ItsPiko
+  Mod Release Date : 5 June 2015
+  
+  By Team Red Eagle.
 */
 
 
