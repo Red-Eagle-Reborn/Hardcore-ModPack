ab3cf4b200e3f5b4b0635d9cababc36e5bf5413c
DevillordMCPE <maulaxe@yahoo.com>
2015-06-06 17:24:32 +0700

    Update Poisonous Raw Food.js

--- Poisonous Raw Food.js
+++ Poisonous Raw Food.js
@@ -1,7 +1,18 @@
 /*
-This code is under the GNU General Public License - Use at free will
-    Credit wanted but not needed - Darkserver -
+                    Red Eagle General License
+                       Version 3, 05/06/2015
+                       
+  You Are Not Allowed To Copying or Using Our Code Without Our Permission.
+  You Are Allowed To Copying or Using Our Code If You Are Have A Permisson From Leader Or Mods Creator.
+  Don't Claim Our Mods To Be Yours.
+  Don't Moditified Out Mods To YOur Version.
+  
+  Mod Creator : DevilLord RedEagle / DevilLord & respectZ
+  Mod Release Date : 05/06/2015
+  
+  By Team Red Eagle.
 */
+
  
 var nowAmount;
 var nowId;
