057940611b4588029c910a429928d979c8d54be1
respectZ <respectZ@users.noreply.github.com>
2015-06-06 14:28:40 +0700

    pre Release [BETA]

--- Stronger Mobs.js
+++ Stronger Mobs.js
@@ -12,13 +12,38 @@
   
   By Team Red Eagle.
 */
+var mobs = [];
+var oldMobs = [];
+var unHostMobs = [64,65,66,77,80,81,82,83,84,85,90];
 function entityAddedHook(e) {
-  Entity.setHealth(e,Entity.getHealth(e)*1.5);
+  for(var t in unHostMobs) {
+  if(Entity.getEntityTypeId(e)!=unHostMobs[t]) {
+    if(e!=getPlayerEnt()) {
+      for(var k in oldMobs) {
+        if(e!=oldMobs[t]) {
+  mobs.push(e);
+        }
+      }
+    }
+  }
+  }
+}
+function modTick () {
+  if(mobs.length!=0) {
+    for(var i=0;i<mobs.length;i++) {
+      var ent = mobs[i];
+      Entity.setHealth(ent,Entity.getHealth(ent)*1.5);
+      mobs.splice(i,0);
+      oldMobs.push(ent);
+    }
+  }
 }
 function attackHook(a,v) {
+  if(v==getPlayerEnt()) {
   if(Entity.getHealth(v)-2>0) {
   Entity.setHealth(v,Math.round(Entity.getHealth(v)-2));
   } else {
     Entity.setHealth(v,1);
+  }
   }
 }
